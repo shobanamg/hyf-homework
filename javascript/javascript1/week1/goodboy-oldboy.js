5e80b35fc11a4f2325d6801898666cdeb69addf0
shobanamg <mathiarulshobana@gmail.com>
2022-10-17 14:02:28 +0100

    calculation of dog year has done

---
+++ javascript/javascript1/week1/goodboy-oldboy.js
@@ -0,0 +1,8 @@
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let dogYear = dogYearFuture - dogYearOfBirth;
+console.log(dogYear);
+
+let shouldShowResultInDogYears = true;
+
+// "Your dog will be 10 human years old in 2027" or "Your dog will be 70 dog years old in 2027"
