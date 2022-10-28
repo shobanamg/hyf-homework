6ad5523ef5f4f587eb07427fb9084e9e5a2b78c2
shobanamg <mathiarulshobana@gmail.com>
2022-10-28 17:43:12 +0100

    refactored code

--- javascript/javascript1/week2/flight-booking-fullname.js
+++ javascript/javascript1/week2/flight-booking-fullname.js
@@ -1,5 +1,5 @@
 function getFullname(firstname, surname) {
-    return firstname+ ' ' + surname;
+  return firstname + " " + surname;
 }
 
 const fullname1 = getFullname("Benjamin", "Hughes");
@@ -9,21 +9,22 @@ console.log(fullname1);
 console.log(fullname2);
 
 //FormalFullname
-function getFormalFullname(firstname, surname, useFormalName=true) {
-    if(useFormalName){
-        return 'lord' + ' ' +  firstname + ' ' + surname;
-    } else {
-    return firstname+ ' ' + surname;
-}
+function getFormalFullname(firstname, surname, useFormalName = true, gender) {
+  if (useFormalName && gender) {
+    if (gender === "male") {
+      return "lord" + " " + firstname + " " + surname;
+    }
+    return "lady" + " " + firstname + " " + surname;
+  }
+  return firstname + " " + surname;
 }
 
-const fullname3 = getFormalFullname("Benjamin", "Hughes",true);
-const fullname4 = getFormalFullname("Shobana", "Mathiarul",false);
+const fullname3 = getFormalFullname("Benjamin", "Hughes", true, "male");
+const fullname4 = getFormalFullname("Benjamin", "Hughes", false);
 const fullname5 = getFormalFullname("", "");
+const fullname6 = getFormalFullname("Shobana", "Mathiarul", true, "female");
 
 console.log(fullname3);
 console.log(fullname4);
 console.log(fullname5);
-
-
-
+console.log(fullname6);
