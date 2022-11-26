b65d34a6d48df67257838524416b89b05af432d3
Mathiarul <mathiarul.gunasekaran@gmail.com>
2022-11-27 00:20:16 +0100

    js2 week3 class work

---
+++ javascript/javascript2/week3/classwork/delayClicker/script.js
@@ -0,0 +1,9 @@
+console.log('hello');
+
+//When the button is clicked it should wait 3 seconds and then log the text "This text was delayed by 3 seconds".
+let button = document.querySelector('.btn')
+
+button.addEventListener('click',
+    () => setTimeout(() => console.log("This text was delayed by 3 seconds"),
+        3000))
+
---
+++ javascript/javascript2/week3/classwork/mousePosition/script.js
@@ -0,0 +1,21 @@
+//First create a callback function as a variable that logs this out: "DOM fully loaded and parsed" This callback
+// function should be called when the DOM is fully loaded.
+// To find what this function is called go to google! What should we search for???
+let position = [];
+
+document.addEventListener('mousemove', (event) => {
+  position.push({x: event.clientX, y: event.clientY})
+})
+
+const positionPrinter = () => {
+  const xAverage = position.reduce((acc, cur) => cur.x + acc, 0)
+      / position.length;
+  const yAverage = position.reduce((acc, cur) => cur.y + acc, 0)
+      / position.length;
+  console.log('The average x and y positions are', Math.floor(xAverage),
+      Math.floor(yAverage));
+  // position = [];
+  // setTimeout(positionPrinter, 3000)
+}
+
+setTimeout(positionPrinter, 3000)
