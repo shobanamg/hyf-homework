839c32c57c40731b4c2f6728b1dadfa462969d7b
Shobana <mathiarulshobana@gmail.com>
2022-11-28 15:56:19 +0100

    refactored code

--- javascript/javascript2/week3/classwork/onload/script.js
+++ javascript/javascript2/week3/classwork/onload/script.js
@@ -1,4 +1,4 @@
 //First create a callback function as a variable that logs this out: "DOM fully loaded and parsed" This callback
 // function should be called when the DOM is fully loaded.
 // To find what this function is called go to google! What should we search for???
-window.onload = () => console.log('DOM fully loaded and parsed');
+window.onload = () => console.log("DOM fully loaded and parsed");
