b212d868f9b1dad99af101fbf5bac8f7cdfbfe88
Shobana <mathiarulshobana@gmail.com>
2022-12-12 13:37:52 +0100

    refactored code

--- javascript/javascript3/week1/classwork/dogFan/script.js
+++ javascript/javascript3/week1/classwork/dogFan/script.js
@@ -1,45 +1,38 @@
 //Lets create a site for dog lovers using this API: https://dog.ceo/api/breeds/image/random
 //1.Get a random dog image and display it in the browser
-// setInterval(() => {
-//   fetch("https://dog.ceo/api/breeds/image/random")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       document.querySelector(
-//         ".image-container"
-//       ).innerHTML = `<img src="${data.message}">`;
-//     });
-// }, 2000);
 
-// const getRandomImage = async (breed) => {
-//   return data.message;
-// };
-//
-// fetch("https://dog.ceo/api/breeds/list/all")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const result = Object.keys(data.message).map(async (breed) => {
-//       let { message } = await fetch(
-//         `https://dog.ceo/api/breed/${breed}/images/random`
-//       )
-//         .then((response) => response.json())
-//         .then((data) => data);
-//       document
-//         .querySelector(".breed-image-container")
-//         .appendChild((document.createElement("img").src = `${message}`));
-//     });
-//     console.log(result);
-//     document.querySelector(".breed-image-container").innerHTML = "";
-//   });
+const getRandomDogImage = () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setTimeout(() => {
+        document.querySelector(
+          ".image-container"
+        ).innerHTML = `<img src="${data.message}" width="500" height="500" alt="dog img">`;
+      });
+    }, 1000);
+};
 
-fetch(" https://dog.ceo/api/breeds/image/random")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.message);
-    const breedName =
-      data.message.split("/")[data.message.split("/").length - 2];
-    console.log(breedName);
-    document.querySelector(
-      ".image-container"
-    ).innerHTML = `<img src="${data.message}" width="500" height="500" alt="dog img">
-<h1>${breedName}</h1>`;
-  });
+const getRandomImage = async () => {
+  const resp = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await resp.json();
+  const breedArr = Object.keys(data.message);
+  const randomIndex = Math.floor(Math.random() * breedArr.length);
+  const randomBreed = breedArr[randomIndex];
+  const url = `https://dog.ceo/api/breed/${randomBreed}/images/random`;
+  const response = await fetch(url);
+  const breed = await response.json();
+  const breedImgContainer = document.querySelector(".breed-image-container");
+  const breedimage = document.createElement("img");
+  const breedName = document.createElement("h2");
+  breedimage.setAttribute("src", `${breed.message}`);
+  breedimage.setAttribute("width", "500");
+  breedimage.setAttribute("height", "500");
+  breedimage.setAttribute("alt", "dog img");
+  breedImgContainer.appendChild(breedimage);
+  breedName.innerHTML = `Breed Name is ${randomBreed}`;
+  breedImgContainer.appendChild(breedName);
+};
+
+setInterval(getRandomImage, 1000);
