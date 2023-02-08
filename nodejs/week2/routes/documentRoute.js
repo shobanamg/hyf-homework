f1c5e69c18ce9db5eaa470a8c13e240775508338
Shobana <mathiarulshobana@gmail.com>
2023-02-08 19:40:47 +0100

    fixed review comments

--- nodejs/week2/routes/documentRoute.js
+++ nodejs/week2/routes/documentRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Document = require("../controllers/documentController");
 
-router.route("/search").get(Document.getDocumentsByQuery);
-
 router.route("/:id").get(Document.getDocumentById);
 
-router.route("/search").post(Document.getDocumentsByFieldsOrQuery);
+router
+  .route("/search")
+  .get(Document.getDocumentsByQuery)
+  .post(Document.getDocumentsByFieldsOrQuery);
 
 module.exports = router;
